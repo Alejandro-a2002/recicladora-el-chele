056162400c02981b031dbb6af7fcf8cde80105de
Alejandro-a2002 <alejandroalvarados2002@gmail.com>
2026-09-17 13:20:38 -0600

    AA13: Configuración para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-//import vue from '@vitejs/plugin-vue'
-//import { defineConfig } from 'vite'
-//
-//// https://vite.dev/config/
-//export default defineConfig({
-//  plugins: [vue()],
-//})
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
     plugins: [vue()],
-    base: '/recicladora-el-chele/'
 })
